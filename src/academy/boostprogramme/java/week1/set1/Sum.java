dde7f5bab1dc3927834b51fd344bb2828594d035
patrizia <patriziagessa@gmail.com>
2020-01-28 21:56:08 +0100

    sum

---
+++ src/academy/boostprogramme/java/week1/set1/Sum.java
@@ -0,0 +1,20 @@
+package academy.boostprogramme.java.week1.set1;
+
+public class Sum{
+    public static int sumInt(int numbOne, int numbTwo) {
+        return numbOne + numbTwo;
+    }
+    public static double sumDouble(double numbOne, double numbTwo) {
+        return numbOne + numbTwo;
+    }
+    public static void main(String[] args) {
+        int sumInt = sumInt(21, 21);
+        double sumDouble = sumDouble(6.1D, 6.1D);
+
+        System.out.println("The int sum is " + sumInt);
+        System.out.println("The double sum is " + sumDouble);
+
+    }
+}
+
+
