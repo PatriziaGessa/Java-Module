8199a2c3e142cb2e4a0c82c7424ce2f0ba7360c9
patrizia <patriziagessa@gmail.com>
2020-01-28 21:11:45 +0100

    easy peasy

--- src/academy/boostprogramme/java/week1/set1/EasyPeasy.java
+++ src/academy/boostprogramme/java/week1/set1/EasyPeasy.java
@@ -1,6 +1,6 @@
 package academy.boostprogramme.java.week1.set1;
 
-public class easyPeasy {
+public class EasyPeasy {
     public static void main(String[] args) {
         System.out.println("Easy peasy!");
 
